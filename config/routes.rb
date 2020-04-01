7bdabb7c349e6218b2d64ddcf75dc626245f7653
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-03-31 17:22:34 -0700

    create nested comments resource within articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   get 'welcome/index'
-  resources :articles
-
+  resources :articles do
+    resources :comments
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
