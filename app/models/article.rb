3fe143b749b785d3137362207ec99a906365c99c
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-03-31 18:17:05 -0700

    delete associated comments when article is deleted

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
 class Article < ActiveRecord::Base
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 5 }
 end
