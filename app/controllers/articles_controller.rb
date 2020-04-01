f3c20883d7a82fc5722be8dc3af2ce3680771fd1
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-03-31 17:10:25 -0700

    add delete method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
-  
+
   def show
     @article = Article.find(params[:id])
   end
@@ -19,23 +21,31 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     if @article.save
       redirect_to @article
-    else 
-    render 'new'
+    else
+      render 'new'
     end
   end
 
   def update
-  @article = Article.find(params[:id])
+    @article = Article.find(params[:id])
 
-  if @article.update(article_params)
-    redirect_to @article
-  else
-    render 'edit'
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
   end
 
   private
-    def article_params
-      params.require(:article).permit(:title, :text)
-    end
+
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 end
